268c5018aa28027cbb9b434872a686f8afcd5871
Regan Sarwas <regan_sarwas@nps.gov>
2015-06-23 12:05:32 -0800

    Added some long unit names for testing

--- app/units.js
+++ app/units.js
@@ -2,24 +2,34 @@
  * Created by regan_sarwas on 6/15/15.
  */
 var units = {
-    'DENA' : {
+    'DENA': {
         'name': 'Denali',
         'type': 'National Park',
         'state': 'Alaska'
     },
-    'BELA' : {
+    'BELA': {
         'name': 'Bering Land Bridge',
         'type': 'National Preserve',
         'state': 'Alaska'
     },
-    'AKRO' : {
+    'AKRO': {
         'name': 'Alaska',
         'type': 'Regional Office',
         'state': 'Headquarters'
     },
-    'SWAN' : {
+    'SWAN': {
         'name': 'South West Alaska',
         'type': 'Inventory & Monitoring Network',
         'state': 'Alaska'
+    },
+    'RORI' : {
+        'name': 'Rosie the Riveter/World War II Home Front',
+        'type': 'National Historical Park',
+        'state': 'California'
+    },
+    'WICL' : {
+        'name': 'President William Jefferson Clinton Birthplace Home',
+        'type': 'National Historical Site',
+        'state': 'Arkansas'
     }
 };
