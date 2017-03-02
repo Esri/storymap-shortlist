9dc6a279fd98d85a93a5e2a485d20f1ba11e3e6a
Regan Sarwas <regan_sarwas@nps.gov>
2017-03-02 11:59:02 -0900

    Support Basemaps with WebTiledLayer or VectorTiledLayer

--- src/app/storymaps/tpl/builder/BuilderView.js
+++ src/app/storymaps/tpl/builder/BuilderView.js
@@ -221,6 +221,24 @@ define(["lib-build/tpl!./BuilderView",
 							delete bmLayerJSON.url;
 							bmLayerJSON.type = "OpenStreetMap";
 						}
+						else if ( layer.type == "VectorTileLayer" ) {
+							delete bmLayerJSON.url;
+							bmLayerJSON.type = "VectorTileLayer";
+							bmLayerJSON.layerType = "VectorTileLayer";
+							bmLayerJSON.title = basemap.title;
+							bmLayerJSON.styleUrl = layer.styleUrl;
+							bmLayerJSON.itemId = basemap.itemId;
+						}
+						else if ( layer.type == "WebTiledLayer" ) {
+							delete bmLayerJSON.url;
+							bmLayerJSON.templateUrl = layer.templateUrl;
+							bmLayerJSON.copyright = layer.copyright;
+							bmLayerJSON.fullExtent = layer.fullExtent;
+							bmLayerJSON.subDomains = layer.subDomains;
+							bmLayerJSON.title = basemap.title;
+							bmLayerJSON.type = "WebTiledLayer";
+							bmLayerJSON.layerType = "WebTiledLayer";
+						}
 
 						newBasemapJSON.push(bmLayerJSON);
 					});
